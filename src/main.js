b21efefbf901b88b0e9fe4c704211a9a110f3923
YSQGOOD <673945225@qq.com>
2025-07-23 18:05:51 +0800

    sddsds

--- src/main.js
+++ src/main.js
@@ -3,10 +3,13 @@ import './assets/index.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { Swipe, SwipeItem } from 'vant';
 // 引入 Vant 及其样式
 import Vant from 'vant'
 import 'vant/lib/index.css'
 const app = createApp(App)
 app.use(router)
 app.use(Vant)
+app.use(Swipe);
+app.use(SwipeItem);
 app.mount('#app')
