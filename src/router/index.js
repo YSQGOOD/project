f062ea115643ede4cef2654b3c1cfa549c871f4f
YSQGOOD <673945225@qq.com>
2025-07-26 16:28:47 +0800

    快快快

--- src/router/index.js
+++ src/router/index.js
@@ -8,20 +8,30 @@ const Grafilab = () => import('../views/grafilab/index.vue')
 const Kerneldao = () => import('../views/kerneldao/index.vue')
 const Kinza = () => import('../views/kinza/index.vue')
 const Ateru = () => import('../views/ateru/index.vue')
+const AteruDetail = () => import('../views/ateru/ateruDetail.vue')
 const routes = [
-  { path: '/', name: 'Hemi', component: Hemi },
-  { path: '/ai', name: 'Ai', component: Ai },
-  { path: '/arcium', name: 'Arcium', component: Arcium },
-  { path: '/astraprotocol', name: 'Astraprotocol', component: Astraprotocol },
-  { path: '/grafilab', name: 'Grafilab', component: Grafilab },
-  { path: '/kerneldao', name: 'Kerneldao', component: Kerneldao },
-  { path: '/kinza', name: 'Kinza', component: Kinza },
-  { path: '/ateru', name: 'Ateru', component: Ateru },
-  
+  // { path: '/', name: 'Hemi', component: Hemi },
+  // { path: '/ai', name: 'Ai', component: Ai },
+  // { path: '/arcium', name: 'Arcium', component: Arcium },
+  // { path: '/astraprotocol', name: 'Astraprotocol', component: Astraprotocol },
+  // { path: '/grafilab', name: 'Grafilab', component: Grafilab },
+  // { path: '/kerneldao', name: 'Kerneldao', component: Kerneldao },
+  // { path: '/kinza', name: 'Kinza', component: Kinza },
+  //{ path: '/ateru', name: 'Ateru', component: Ateru },
+
+  // { path: '/', name: 'Hemi', component: Hemi },
+  // { path: '/', name: 'Ai', component: Ai },
+  // { path: '/', name: 'Arcium', component: Arcium },
+  // { path: '/', name: 'Astraprotocol', component: Astraprotocol },
+  // { path: '/', name: 'Grafilab', component: Grafilab },
+  // { path: '/', name: 'Kerneldao', component: Kerneldao },
+  // { path: '/', name: 'Kinza', component: Kinza },
+  { path: '/', name: 'Ateru', component: Ateru },
+  { path: '/ateruDetail', name: 'AteruDetail', component: AteruDetail },
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes
 })
 
---
+++ src/views/ateru/ateruDetail.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="ateru-page">
+    <object
+      :data="htmlPath"
+      ref="webPage"
+      type="text/html"
+      style="width: 100%; height: 99%;"
+    ></object>
+  </div>
+</template>
+
+<script setup>
+import {
+  ref,
+  onMounted,
+} from "vue";
+const htmlPath = ref('');
+
+onMounted(() => {
+  htmlPath.value = `${import.meta.env.BASE_URL}external/html/ateruDetail.html`;
+});
+
+
+
+</script>
+
+<style scoped>
+.ateru-page {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+}
+
+</style>
